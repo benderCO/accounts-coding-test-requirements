1733a9f10aa7074669227de98bb796921a0bac2b
unknown <mpwester@gmail.com>
2020-06-30 20:54:49 -0500

    I'm really really dumb

--- frontier/src/app/app.component.ts
+++ frontier/src/app/app.component.ts
@@ -22,11 +22,9 @@ export class AppComponent {
     this.accountsServiceService.loadAllAccounts().pipe(
       tap((resp: []) => {
         console.log('Responses are: ', resp);
-        var currDate = new Date();
-        currDate.setMonth(currDate.getMonth() + 1);
-        this.activeAccounts = resp.filter((item: any) => item.PaymentDueDate < currDate) as any;
-        this.overdueAccounts = resp.filter((item: any) => item.PaymentDueDate >= currDate) as any;
-        this.inactiveAccounts = resp.filter((item: any) => item.PaymentDueDate == null) as any;
+        this.activeAccounts = resp.filter((item: any) => item.AccountStatusId === 0) as any;
+        this.overdueAccounts = resp.filter((item: any) => item.AccountStatusId === 1) as any;
+        this.inactiveAccounts = resp.filter((item: any) => item.AccountStatusId === 2) as any;
       })
     ).subscribe();
   }
